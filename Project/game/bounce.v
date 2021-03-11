b3c85afcc767162126aff38ca07498c59338fb76
VickeyZ <80439363+VickeyZ@users.noreply.github.com>
2021-03-11 11:58:27 +0800

    Add files via upload

---
+++ Project/game/bounce.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:42:15 01/05/2020 
+// Design Name: 
+// Module Name:    bounce 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module bounce(
+	input wire clk,
+	input wire start,
+	input wire reset,
+	input wire [3:0] max_speed,
+	output reg [9:0] x,
+	output reg [8:0] y,
+	output reg [3:0] speed,
+	output reg direction
+    );
+	
+	initial begin
+		x <= 10'd276;
+		y <= 9'd320;
+		speed <= 7;
+		direction <= 1;
+	end
+	
+	always @(posedge clk or negedge reset) begin
+		if(reset == 0)begin
+			if(start == 0)begin
+				x <= 10'd276;
+				y <= 9'd320;
+				speed <= 7;
+				direction <= 1;
+			end
+		end
+		else begin
+			if(start == 1) begin
+				if(direction == 1)
+					y <= y - speed;
+				else
+					y <= y + speed;
+				if(speed == 0)
+					direction <= ~direction;
+				if(direction == 1 && speed != 0 || speed == 0 && direction == 0)begin
+					if(speed == 0)
+						speed <= max_speed;
+					else
+						speed <= speed - 1;
+				end 
+				else begin
+					if(speed == 14 - max_speed)
+						speed <= 0;
+					else
+						speed <= speed + 1;
+				end
+			end
+		end
+	end
+
+endmodule
+
---
+++ Project/game/change_speed.v
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:42:39 01/05/2020 
+// Design Name: 
+// Module Name:    change_speed 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module change_speed(
+	input wire clk,
+	input wire direction,
+	input wire change,
+	input wire start,
+	input wire reset,
+	input wire collision,
+	input wire [3:0] ball_speed,
+	input wire [6:0] height_5,
+	input wire [6:0] height_6,
+	input wire [6:0] height_7,
+	output reg [3:0] barrier_speed,
+	output reg [3:0] max_speed
+    );
+	
+	initial begin
+		max_speed <= 7;
+		barrier_speed <= 4;
+	end
+	
+	always @(posedge clk or negedge change or negedge reset)begin
+		if(reset == 0)begin
+			if(start == 0 || collision == 1)begin
+				max_speed <= 7;
+				barrier_speed <= 4;
+			end
+		end
+		else begin
+			if(change == 0)begin
+				if(start == 1)begin
+					if(ball_speed == 0 && direction == 0 && height_5 >= height_6)
+						barrier_speed <= 8;
+					else if(ball_speed == 0 && direction == 0)
+						barrier_speed <= 4;
+						
+					if(ball_speed == 0 && direction == 0 && height_5 >= height_6 && height_5 > height_7)
+						max_speed <= 5; 
+					else if(ball_speed == 0 && direction == 0 && height_6 > height_5)
+						max_speed <= 9;
+					else if(ball_speed == 0 && direction == 0)
+						max_speed <= 7;
+				end
+			end
+			else begin
+				if(start == 1)begin
+					if(ball_speed == 0 && direction == 0 && height_6 < height_5)begin
+						max_speed <= 5;
+						barrier_speed <= 4;
+					end
+					else begin
+						if(ball_speed == 0 && direction == 0)begin
+							max_speed <= 7;
+							barrier_speed <= 4;
+						end
+					end
+				end
+			end
+		end
+	end
+
+endmodule
+
